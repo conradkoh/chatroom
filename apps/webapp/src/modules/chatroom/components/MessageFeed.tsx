dbe7ddbc86b2cefea5ad7fce0b2f721e34ef4715
conradkoh <conradkoh@gmail.com>
2026-01-21 16:50:06 +0800

    fix(ui): remove top padding from scroll container for flush sticky headers
    Changed scroll container padding from `p-4` to `pt-0 px-4 pb-4`
    so sticky TaskHeaders stick flush to the container top.

--- apps/webapp/src/modules/chatroom/components/MessageFeed.tsx
+++ apps/webapp/src/modules/chatroom/components/MessageFeed.tsx
@@ -648,8 +648,9 @@ export const MessageFeed = memo(function MessageFeed({
   return (
     <div className="flex-1 flex flex-col min-h-0 relative">
       {/* Scrollable message content - Task headers use CSS sticky to stay at top while scrolling */}
+      {/* Note: pt-0 ensures sticky headers stick flush to container top */}
       <div
-        className="flex-1 overflow-y-auto p-4 min-h-0 scrollbar-thin scrollbar-track-chatroom-bg-primary scrollbar-thumb-chatroom-border"
+        className="flex-1 overflow-y-auto pt-0 px-4 pb-4 min-h-0 scrollbar-thin scrollbar-track-chatroom-bg-primary scrollbar-thumb-chatroom-border"
         ref={feedRef}
         onScroll={handleScroll}
       >
